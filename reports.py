d6bd5e802b0eb830308918a8ac8d0fbc5ec0fb40
Muhammad <moli1369@gmail.com>
2025-02-01 00:46:23 +0330

    new gui and fix bug

--- reports.py
+++ reports.py
@@ -1,12 +1,14 @@
 import pandas as pd
+from course_management import load_courses  # وارد کردن تابع load_courses
+from grading import load_grades  # وارد کردن تابع load_grades
 
 # تولید لیست دانشجویان و نمرات آن‌ها
 def generate_class_list(course_id):
-    courses = load_courses()
-    grades = load_grades()
+    courses = load_courses()  # بارگیری اطلاعات دوره‌ها
+    grades = load_grades()  # بارگیری اطلاعات نمرات
     
     if course_id not in courses or course_id not in grades:
-        return None
+        return None  # اگر دوره یا نمرات وجود نداشته باشد، None برگردان
     
     students_ids = courses[course_id]['students_ids']
     grading_system = grades[course_id]['grading_system']
